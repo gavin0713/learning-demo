d33ea9c0044a76a15027a6a6d0a140c1b5a8cc16
Gavin <while@whu.edu.cn>
2021-09-03 22:07:12 +0800

    change project struct

--- rpc/example/rpc-client/src/main/java/com/coder/rpcclient/RpcClientApplication.java
+++ rpc/example/rpc-client/src/main/java/com/coder/rpcclient/RpcClientApplication.java
@@ -1,11 +1,8 @@
 package com.coder.rpcclient;
 
 import com.coder.grpc.annotation.EnableGrpcClient;
-import com.coder.grpc.client.ReferenceInvokeProxy;
-import com.coder.grpc.client.RemoteInvocationHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableGrpcClient
--- rpc/example/rpc-server/src/main/java/com/example/rpcserver/RpcServerApplication.java
+++ rpc/example/rpc-server/src/main/java/com/example/rpcserver/RpcServerApplication.java
@@ -1,11 +1,8 @@
 package com.example.rpcserver;
 
 import com.coder.grpc.annotation.EnableGrpcServer;
-import com.coder.grpc.server.InitialMediator;
-import com.coder.grpc.server.SocketServerInitial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableGrpcServer
